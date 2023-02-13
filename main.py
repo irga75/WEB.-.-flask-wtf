bad8f067637bed15872230e416714640ee89a7f7
irga75 <ira.gurzhy@yandex.ru>
2023-02-13 19:08:27 +0300

    Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,14 @@ def prof_practice(prof):
     return render_template('prof.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def return_list_prof(list):
+    prof_list = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                 'инженер по терраформированию', 'климатолог', 'специалист по радиационной защите',
+                 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения', 'метеоролог',
+                 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
+    return render_template("prof_list.html", list_type=list, prof_list=prof_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
