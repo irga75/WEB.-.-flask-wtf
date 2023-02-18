99b817228d162c8b6ebb77c3e58c8f4458cfb959
irga75 <ira.gurzhy@yandex.ru>
2023-02-18 13:42:29 +0300

    Stage

--- main.py
+++ main.py
@@ -72,5 +72,10 @@ def distribute():
     return render_template('distribution.html', dist_list=dist_list, title='Каютное распределение')
 
 
+@app.route('/table/<sex>/<age>')
+def return_table(sex, age):
+    return render_template('table.html', sex=sex, age=age, title='Оформление кают')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
